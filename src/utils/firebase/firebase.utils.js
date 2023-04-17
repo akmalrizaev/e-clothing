80e1819d1925ecde7be3d31173e999a59a97caed
Akmal Rizayev <akmal.rizayev@gmail.com>
2023-04-17 17:03:31 +0500

    Authenticating With Firebase

---
+++ src/utils/firebase/firebase.utils.js
@@ -0,0 +1,36 @@
+import { initializeApp } from 'firebase/app';
+
+import {
+  getAuth,
+  signInWithRedirect,
+  signInWithPopup,
+  GithubAuthProvider,
+  GoogleAuthProvider,
+} from 'firebase/auth';
+
+// Your web app's Firebase configuration
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyC3AM_vUYQRNarpDslElrt3nMfuIMyCE7Y',
+
+  authDomain: 'e-clothing-db-4665e.firebaseapp.com',
+
+  projectId: 'e-clothing-db-4665e',
+
+  storageBucket: 'e-clothing-db-4665e.appspot.com',
+
+  messagingSenderId: '715775214184',
+
+  appId: '1:715775214184:web:f2f4de64a27ac9125271dd',
+};
+
+// Initialize Firebase
+
+const firebaseApp = initializeApp(firebaseConfig);
+const provider = new GoogleAuthProvider();
+provider.setCustomParameters({
+  prompt: 'select_account',
+});
+
+export const auth = getAuth();
+export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
