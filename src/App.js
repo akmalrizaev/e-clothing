dde13f7cf24fac7ea4fd1f3351430e3914175251
Akmal Rizayev <akmal.rizayev@gmail.com>
2023-04-17 16:23:45 +0500

    Setting Up Firebase

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import Navigation from './routes/navigation/navigation.component';
 
 import Home from './routes/home/home.component';
 
+import SignIn from './routes/sign-in/sign-in.component';
+
 const Shop = () => {
   return <h1>I am Shop component</h1>;
 };
@@ -41,6 +43,7 @@ const App = () => {
       <Route path="/" element={<Navigation />}>
         <Route index element={<Home />} />
         <Route path="shop" element={<Shop />} />
+        <Route path="sign-in" element={<SignIn />} />
       </Route>
     </Routes>
   );
--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -15,6 +15,10 @@ const Navigation = () => {
           <Link className="nav-link" to="/shop">
             Shop
           </Link>
+
+          <Link className="nav-link" to="/sign-in">
+            Sign In
+          </Link>
         </div>
       </div>
       <Outlet />
