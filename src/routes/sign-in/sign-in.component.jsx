ee2d1366d8c44b576d5d94dcc7db422306e2c97d
Akmal Rizayev <akmal.rizayev@gmail.com>
2023-04-18 15:37:35 +0500

    Sign In With Redirect

--- src/routes/sign-in/sign-in.component.jsx
+++ src/routes/sign-in/sign-in.component.jsx
@@ -1,11 +1,17 @@
-import { signInWithGooglePopup } from '../../utils/firebase/firebase.utils';
-import { createUserDocumentFromAuth } from '../../utils/firebase/firebase.utils';
+import {
+  signInWithGooglePopup,
+  createUserDocumentFromAuth,
+} from '../../utils/firebase/firebase.utils';
 
 const SignIn = () => {
   const logGoogleUser = async () => {
     const { user } = await signInWithGooglePopup();
     const userDocRef = await createUserDocumentFromAuth(user);
   };
+
+  //   const logGoogleRedirectUser = async () => {
+  //     const { user } = await signInWithGoogleRedirect();
+  //   };
   return (
     <div>
       <h1>Sign In Page</h1>
